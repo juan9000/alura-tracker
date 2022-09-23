6cf801a54644017897096a2612aef5de43f4b7f8
Juan Matheus <juanfideli@hotmail.com>
2022-09-23 01:24:15 -0300

    Added the button to edit a project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="projects">
     <h1 class="title">Projects</h1>
-    <router-link to="/projetos/novo" class="button">
+    <router-link to="/projects/new" class="button">
       <span class="icon is-small">
         <i class="fas fa-plus"></i>
       </span>
-      <span>Novo projeto</span>
+      <span>New project</span>
     </router-link>
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Name</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -21,6 +22,13 @@
           </td>
           <td>
             {{ project.name }}
+          </td>
+          <td>
+            <router-link :to="`/projects/${project.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
           </td>
         </tr>
       </tbody>
