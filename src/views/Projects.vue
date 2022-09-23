c1bc2507298ee9ce7ba6811e952c90010b454d7f
Juan Matheus <juanfideli@hotmail.com>
2022-09-23 00:17:04 -0300

    Created a interface for project and finished the table for projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,22 +1,64 @@
 <template>
-  <section>
+  <section class="projects">
     <h1 class="title">Projects</h1>
-    <form>
+    <form @submit.prevent="save">
       <div class="field">
         <label for="projectName" class="label"> Project Name </label>
         <input type="text" class="input" v-model="projectName" />
-        <div class="field">
+        <div class="field mt-2">
           <button class="button" type="submit">Save</button>
         </div>
       </div>
     </form>
+
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>
+            {{ project.id }}
+          </td>
+          <td>
+            {{ project.name }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IProject from "../interfaces/IProject";
 
 export default defineComponent({
   name: "Projects",
+  data() {
+    return {
+      projectName: "",
+      projects: [] as IProject[],
+    };
+  },
+  methods: {
+    save() {
+      const project: IProject = {
+        name: this.projectName,
+        id: new Date().toISOString(),
+      };
+      this.projects.push(project);
+      this.projectName = "";
+    },
+  },
 });
 </script>
+
+<style scoped>
+.projects {
+  padding: 1.25rem;
+}
+</style>
